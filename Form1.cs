2e4d992f4e84fd8c1a0d7a4f5552047939447e1e
pocmukc <pocmukc@alt.ru>
2013-03-18 15:06:07 +0700

    Разбиение по ";"

--- Form1.cs
+++ Form1.cs
@@ -15,5 +15,32 @@ namespace JSON_to_dict
         {
             InitializeComponent();
         }
+
+        #region Тестовые строки
+
+        static string test1 = "{\n" +
+                "\"id\" : 234,\n" + 
+                "\"userid\": 2412,\n" + 
+                "\"money\": 20.7,\n" + 
+                "\"phone_accepted\": true,\n" + 
+                "\"rubrics\":\n" +
+                "\t[ 12,14,16 ]," + 
+                "\"services\":\n" +
+                "\t[ 12,14,16 ]\n" + 
+                "}";
+
+        static string test2 = "{ \"id\" : 2354, \"status\" :true, "+
+            "\"message\" : \"text\", \"phone\" : \"+323424343\", "+
+            "\"workrequestid\" : 123, \"staff_money\" : 47.50 }";
+
+        static string test3 = "{\"id\" : 800, \"workrequestid\": 1234, \"status\": 12435}";
+
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int key = test3.IndexOf('"');
+            MessageBox.Show(test3.Substring(key+1));
+        }
     }
 }
--- JSONParser.cs
+++ JSONParser.cs
@@ -7,5 +7,43 @@ namespace JSON_to_dict
 {
     class JSONParser
     {
+        private string findString(string val)
+        { 
+            return val;
+        }
+
+        private string findInteger(string val)
+        {
+            return val;
+        }
+
+        private string findBool(string val)
+        {
+            return val;
+        }
+
+        public Dictionary<string, string> parse(string json)
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+
+            var lines = json.Split(';');
+
+            foreach (string line in lines)
+            {
+                int key_pos = line.IndexOf('"');
+
+                if (key_pos > -1)
+                {
+                    string key = line.Substring(key_pos+1);
+                    key_pos = key.IndexOf('"');
+                    key = key.Substring(0, key_pos);
+
+                    string val = line.Substring(2 + key.Length);
+
+                }
+            }
+
+            return result;
+        }
     }
 }
