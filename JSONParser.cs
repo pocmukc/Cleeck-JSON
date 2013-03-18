ea1a80c3427c2c375245810434bf91caadf02e5d
pocmukc <pocmukc@alt.ru>
2013-03-18 16:24:08 +0700

    Посимвольный переход по строке foreach (ch in string)

--- JSONParser.cs
+++ JSONParser.cs
@@ -7,39 +7,107 @@ namespace JSON_to_dict
 {
     class JSONParser
     {
-        private string findString(string val)
+        enum states 
         { 
-            return val;
-        }
-
-        private string findInteger(string val)
-        {
-            return val;
-        }
-
-        private string findBool(string val)
-        {
-            return val;
-        }
+            start
+            key,
+            val,
+            val_string,
+            val_int,
+            val_bool,
+            val_array,
+        };
 
         public Dictionary<string, string> parse(string json)
         {
             Dictionary<string, string> result = new Dictionary<string, string>();
 
-            var lines = json.Split(';');
+            states state = states.start; // состояния
 
-            foreach (string line in lines)
-            {
-                int key_pos = line.IndexOf('"');
+            bool flag = false; // флаг записи
 
-                if (key_pos > -1)
+            string key ="";
+            string val ="";
+
+            foreach (char ch in json)
+            { 
+                switch (state)
                 {
-                    string key = line.Substring(key_pos+1);
-                    key_pos = key.IndexOf('"');
-                    key = key.Substring(0, key_pos);
+                    case states.start:
+                        flag = false; 
+                        key ="";
+                        val ="";
+                        goto case states.key;
 
-                    string val = line.Substring(2 + key.Length);
+                    #region Поиск ключа
 
+                    case states.key: 
+                        if (flag) // если записываем
+                        {
+                            if (ch != '"') // пишем пока не закроется "
+                            {
+                                key = key + ch;
+                            }
+                            else
+                            {
+                                // запрещаем запись и устанавливаем 
+                                // состояние в поиск значения
+                                state = states.val;
+                                flag = false;
+                            }
+                        }
+                        else
+                        {
+                            if (ch == '"') // нашли начало ключа
+                            {
+                                flag = true;
+                            }
+                        }
+                        break;
+
+                    #endregion
+
+                    #region Поиск значения
+
+                    case states.val:
+                        if ((ch - '0' >= 0) &&
+                            ((ch - '0' <= '9')))
+                        {
+                            state = states.val_int;
+                            val = val + ch;
+                        }
+                        switch (ch)
+                        {
+                            case '"':
+                                state = states.val_string;
+                                break;
+                            case '[':
+                                state = states.val_array;
+                                break;
+                            case 'b':
+                                state = states.val_bool;
+                                flag = true;
+                                break;
+                            case 'f':
+                                state = states.val_bool;
+                                flag = false;
+                                break;
+                        }
+                        break;
+
+                    #endregion
+
+                    case states.val_bool:
+                        if (flag)
+                            result.Add(key, "true");
+                        else
+                            result.Add(key, "false");
+                        state = states.start;
+                        break;
+
+                    case states.val_string:
+
+                        break;
                 }
             }
 
